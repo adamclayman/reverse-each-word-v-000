90f5e5b309ab4f2862efa8a17c06d62b62c26d78
Adam Clayman <adam@clayman.org>
2017-10-16 05:25:25 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,11 @@
+require 'pry'
+
 def reverse_each_word(string)
   array = string.split(" ")
   reversed_array = array.collect do |word|
     word.reverse
   end
+  binding.pry
   reversed_string = reversed_array.join()
   return reversed_string
 end
